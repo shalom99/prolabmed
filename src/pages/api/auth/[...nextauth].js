0cb8a042e28e8685ee9fbb348166bd6717c20938
shalom99 <castroshalom77@gmail.com>
2023-06-12 23:00:58 +0800

    reverted footer v2

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 
 import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "@/lib/prisma";
+// import prisma from "@/lib/prisma";
 
 export const authOptions = {
   // Configure one or more authentication providers
